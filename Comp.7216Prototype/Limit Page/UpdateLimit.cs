e0b66b34670a3afd4713ebfd419bef1c3983807c
Conner Pearce <metalichking@outlook.com>
2020-06-04 15:00:53 +1200

    More on Limit Design

--- Comp.7216Prototype/Limit Page/UpdateLimit.cs
+++ Comp.7216Prototype/Limit Page/UpdateLimit.cs
@@ -16,5 +16,17 @@ namespace Comp._7216Prototype.Limit_Page
         {
             InitializeComponent();
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            LimitHome limitHome = new LimitHome();
+            limitHome.Show();
+            Hide();
+        }
     }
 }
--- Comp.7216Prototype/MainPage.Designer.cs
+++ Comp.7216Prototype/MainPage.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.btnCredit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCredit
             // 
-            this.btnCredit.Location = new System.Drawing.Point(12, 107);
+            this.btnCredit.Location = new System.Drawing.Point(66, 129);
             this.btnCredit.Name = "btnCredit";
             this.btnCredit.Size = new System.Drawing.Size(113, 40);
             this.btnCredit.TabIndex = 0;
@@ -41,20 +42,33 @@
             this.btnCredit.UseVisualStyleBackColor = true;
             this.btnCredit.Click += new System.EventHandler(this.btnCredit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Home Page";
+            // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(297, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCredit);
             this.Name = "MainPage";
             this.Text = "MainPage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnCredit;
+        private System.Windows.Forms.Label label1;
     }
 }
