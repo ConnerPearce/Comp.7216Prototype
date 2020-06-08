c0359d8686fa984aaae0ec08e512f88e9b9dae51
Conner Pearce <metalichking@outlook.com>
2020-06-08 14:42:38 +1200

    More Limit Updates

--- Comp.7216Prototype/Limit Page/LimitHome.Designer.cs
+++ Comp.7216Prototype/Limit Page/LimitHome.Designer.cs
@@ -31,6 +31,8 @@
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnCreate = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnRemove
@@ -59,18 +61,42 @@
             this.btnUpdate.TabIndex = 2;
             this.btnUpdate.Text = "Update Limit";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Limit Home";
             // 
             // LimitHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(243, 413);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.btnRemove);
             this.Name = "LimitHome";
             this.Text = "LimitHome";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,5 +105,7 @@
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
     }
 }
