277a0844ecbb5fdfbcc531f6fb17a97cf9b5b980
Conner Pearce <metalichking@outlook.com>
2020-06-15 14:21:20 +1200

    Added limit record model

---
+++ Comp.7216Prototype/Database Files/Data/LimitRecord.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files.Data
+{
+    public class LimitRecord
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public int Limit { get; set; }
+        public DateTime MyProperty { get; set; }
+        public string UserID { get; set; }
+    }
+}
