b0a826d9214f244ba2f9b07862dc01cc6ab8a3ca
Agassi Shaju <mail.agassi@gmail.com>
2020-06-15 15:17:29 +1200

    updated---AG

---
+++ Comp.7216Prototype/Database Files/Data/PaymentDetails.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files.Data
+{
+    class PaymentDetails
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public int CardCVV { get; set; }
+        public int CardDetails { get; set; }
+        public string CardHolderName { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
