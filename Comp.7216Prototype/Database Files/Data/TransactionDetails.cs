3eb2881042cd573f25cedc4772d677cafaa6d63c
Jot Sidhu <27036778@student.toiohomai.ac.nz>
2020-06-15 14:30:21 +1200

    Added Transaction data model

---
+++ Comp.7216Prototype/Database Files/Data/TransactionDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files.Data
+{
+    public class TransactionDetails
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+    }
+}
