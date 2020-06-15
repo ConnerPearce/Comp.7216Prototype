27aa7b1ab62568a2b0d92cd81fbf3a28691a28b1
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2020-06-15 14:26:47 +1200

    Model class for PayeeDetails created

---
+++ Comp.7216Prototype/Database Files/Data/PayeeDetails.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files.Data
+{
+    public class PayeeDetails
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string UserName { get; set; }
+        public string Customer { get; set; }
+    }
+}
