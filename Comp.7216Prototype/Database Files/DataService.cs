56f544bf8c75c2a2cd88893823cab66860f71968
Conner Pearce <metalichking@outlook.com>
2020-06-15 15:21:38 +1200

    Added DataService

---
+++ Comp.7216Prototype/Database Files/DataService.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files
+{
+    public class DataService
+    {
+        private IMongoDatabase db;
+
+        public DataService()
+        {
+            var client = new MongoClient("mongodb+srv://admin:admin@prototype-iuehj.mongodb.net/");
+            db = client.GetDatabase("prototypedatabase");
+        }
+
+
+    }
+}
