d4eacae129097488f3e1e0d5da414f1e9096a5b2
Noxi Cola <karl.perez94@gmail.com>
2020-06-15 14:32:27 +1200

    Added RecordMangement class to Database > Data folder

---
+++ Comp.7216Prototype/Database Files/Data/RecordManagement.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp._7216Prototype.Database_Files.Data
+{
+    public class RecordManagement
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string CustomerID { get; set; }
+        public string PaymentID { get; set; }
+        public string TransferID { get; set; }
+        public string TransactionID { get; set; }
+    }
+}
